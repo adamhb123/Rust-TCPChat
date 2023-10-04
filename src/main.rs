9174a36aa8a0b850fe08bab10aa6675d18d21919
Adam Brewer <adamhb321@gmail.com>
2023-10-04 15:10:52 -0400

    Latest

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 use std::env;
 use std::io::prelude::*;
-use std::net::TcpStream;
+use std::net::*;
 
 fn run_client(host: &str){
     println!("Initializing client...");
-    println!("Connecting to host: {0}", host);
-}
+    println!("Connecting to host: {:?}", host);
+    let mut sock_stream : TcpStream = match TcpStream::connect(host) {
+        Ok(sock_stream) => sock_stream,
+        Err(err) => panic!("Error connecting to server {0}\n{1}", host, err)
+    };
+    println!("Successfully connected to host: {:?}", host);
+    println!("Sending message 'Test'");
+    sock_stream.write_all("Test".as_bytes()).unwrap();
+
+ }
 
 fn run_server(host: &str){
     println!("Initializing server...");
+    let listener :TcpListener  = match TcpListener::bind(host) {
+        Ok(listener) => listener,
+        Err(err) => panic!("Error binding server: {:?}", err)
+    };
+    let (stream, client_addr): (TcpStream, SocketAddr) = match listener.accept() {
+        Ok((stream, client_addr)) => (stream, client_addr),
+        Err(err) => panic!("Error accepting client: {:?}", err)
+    };
+    println!("Client: {:?} successfully connected", client_addr);
 }
 
 fn check_args(args: &Vec<String>) -> bool {
-    if 
-    return true;
+    if args.len() > 2 {
+        return true;
+    }
+    false
 }
 
 fn main() {
